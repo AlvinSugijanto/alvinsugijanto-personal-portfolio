1ff622f7be987622cee9da40780fcbac110ed68d
Alvin <alvin.sugijanto@gmail.com>
2024-04-02 19:08:22 +0700

    update social media section

--- src/components/SocialMedia.jsx
+++ src/components/SocialMedia.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useTheme } from "../ThemeProvider";
 import SwiperComponent from "../Swiper";
-import { FaGithub } from "react-icons/fa";
+import { FaGithub, FaInstagram } from "react-icons/fa";
 import { CgMail } from "react-icons/cg";
 import { FaLinkedin } from "react-icons/fa";
 
@@ -14,29 +14,33 @@ function SocialMedia() {
     const SOCIAL_MEDIA_LIST = [
 
         {
-            href : 'https://github.com/AlvinSugijanto',
-            icon : <FaGithub className={`${themeClass.textPrimary}`}/>
+            href: 'https://github.com/AlvinSugijanto',
+            icon: <FaGithub className={`${themeClass.textPrimary}`} />
         },
         {
-            href : 'https://github.com/AlvinSugijanto',
-            icon : <CgMail className={`${themeClass.textPrimary}`}/>
+            href: 'https://www.instagram.com/alvinsugijaannto/',
+            icon: <FaInstagram className={`${themeClass.textPrimary}`} />
         },
         {
-            href : 'https://github.com/AlvinSugijanto',
-            icon : <FaLinkedin className={`${themeClass.textPrimary}`}/>
+            href: 'https://www.linkedin.com/in/alvinsugijanto/',
+            icon: <FaLinkedin className={`${themeClass.textPrimary}`} />
         }
 
     ]
 
     return (
         <>
-            <div className="fixed top-2/3 left-1.5 -z-1">
+            <div className="fixed top-2/3 left-1.5 z-50">
                 <div className="flex flex-col gap-3">
 
                     {SOCIAL_MEDIA_LIST.map((item, index) => (
-                        <div key={index} className={`rounded-full p-2 backdrop-blur-xl border ${themeClass.backgroundSecondary} ${themeClass.borderColor} hover:bg-slate-200 hover:cursor-pointer`}>
+                        <a
+                            key={index}
+                            className={`rounded-full p-2 backdrop-blur-xl border ${themeClass.backgroundSecondary} ${themeClass.borderColor} hover:bg-slate-200 hover:cursor-pointer`}
+                            onClick={() => window.open(item.href, '_blank')}
+                        >
                             {item.icon}
-                        </div>
+                        </a>
                     ))}
 
                 </div>
